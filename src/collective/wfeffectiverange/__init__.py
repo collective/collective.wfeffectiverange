bc41f369065506c6fe606dd88f90429a33c49902
Peter Holzer <hpeter@agitator.com>
2014-12-13 13:50:34 +0100

    added translations for german

--- src/collective/wfeffectiverange/__init__.py
+++ src/collective/wfeffectiverange/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from zope.i18nmessageid import MessageFactory
 
-_ = MessageFactory('plone')
+_ = MessageFactory('collective.wfeffectiverange')
---
+++ update_locales.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+I18NDOMAIN="collective.wfeffectiverange"
+PACKAGENAME="src/collective/wfeffectiverange"
+
+# Synchronise the templates and scripts with the .pot.
+# All on one line normally:
+../../bin/i18ndude rebuild-pot --pot `pwd`/${PACKAGENAME}/locales/${I18NDOMAIN}.pot \
+    --create ${I18NDOMAIN} \
+   .
+
+# Synchronise the resulting .pot with all .po files
+for po in `pwd`/${PACKAGENAME}/locales/*/LC_MESSAGES/${I18NDOMAIN}.po; do
+    ../../bin/i18ndude sync --pot `pwd`/${PACKAGENAME}/locales/${I18NDOMAIN}.pot $po
+done
