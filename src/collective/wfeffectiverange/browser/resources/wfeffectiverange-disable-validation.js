1b9eb58e89d2e6a6d8c45c496a561081ad3c2a1d
Jens W. Klein <jk@kleinundpartner.at>
2016-10-31 15:05:33 +0100

    apply jscs formatter to js files

--- src/collective/wfeffectiverange/browser/resources/wfeffectiverange-disable-validation.js
+++ src/collective/wfeffectiverange/browser/resources/wfeffectiverange-disable-validation.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
 
   //  disable default validation
-  $('#formfield-form-widgets-IWFEffectiveRange-effective').removeClass('z3cformInlineValidation');
-  $('#formfield-form-widgets-IWFEffectiveRange-expires').removeClass('z3cformInlineValidation');
-  $('#formfield-form-widgets-IWFEffectiveRange-effective_transition').removeClass('z3cformInlineValidation');
-  $('#formfield-form-widgets-IWFEffectiveRange-expires_transition').removeClass('z3cformInlineValidation');
+  $('#formfield-form-widgets-IWFEffectiveRange-effective').removeClass(
+    'z3cformInlineValidation'
+  );
+  $('#formfield-form-widgets-IWFEffectiveRange-expires').removeClass(
+    'z3cformInlineValidation'
+  );
+  $('#formfield-form-widgets-IWFEffectiveRange-effective_transition').removeClass(
+    'z3cformInlineValidation'
+  );
+  $('#formfield-form-widgets-IWFEffectiveRange-expires_transition').removeClass(
+    'z3cformInlineValidation'
+  );
 
 });
 
