30689d43c04b8d675bd42dc57e56f7025f4f5f85
Johannes Raggam <thetetet@gmail.com>
2017-08-24 14:04:03 +0200

    ADD WFTask support pt6

--- src/collective/wfeffectiverange/browser/wftasks.py
+++ src/collective/wfeffectiverange/browser/wftasks.py
@@ -1,11 +1,18 @@
 # -*- coding: utf-8 -*-
 from plone.app.contenttypes.browser.folder import FolderView
+from plone.app.uuid.utils import uuidToObject
+from plone.protect.utils import addTokenToUrl
+from plone.uuid.interfaces import IUUID
+from zExceptions import Redirect
 
 import plone.api
 
 
 class WFTasksOverviewView(FolderView):
 
+    def protected_view_url(self):
+        url = addTokenToUrl(self.context.absolute_url() + '/@@workflow-tasks')
+        return url
 
     def tasks_effective(self):
         ret = plone.api.content.find(**{
@@ -19,22 +26,33 @@ class WFTasksOverviewView(FolderView):
         })
         return ret
 
+    def task_info(self, task):
+        return {
+            'title': task.title,
+            'url': task.absolute_url(),
+            'date': task.task_date,
+            'transition': task.task_transition,
+            'uuid': IUUID(task)
+        }
+
     def task_objects(self, task):
         refs = getattr(task, 'task_items', [])
         ret = [ref.to_object for ref in refs if ref]
         return ret
 
     def task_objects_info(self, task):
-        objects = self.task_objects(task)
         wftool = plone.api.portal.get_tool('portal_workflow')
 
         ret = []
-        for ob in objects:
+        refs = getattr(task, 'task_items', [])
+        for ref in refs:
+            ob = ref.to_object
             ret.append({
                 'title': ob.title,
                 'url': ob.absolute_url(),
                 'workflows': u', '.join([wf.title for wf in wftool.getWorkflowsFor(ob)]),  # noqa
-                'state': plone.api.content.get_state(ob)
+                'state': plone.api.content.get_state(ob),
+                'intid': ref.to_id
             })
 
         return ret
@@ -57,3 +75,28 @@ class WFTasksOverviewView(FolderView):
             ret = ret.intersection(transition)
 
         return list(ret)
+
+    def __call__(self, *args, **kwargs):
+
+        form = self.request.form
+        task_id = form.get('task_id')
+
+        if task_id:
+            task = uuidToObject(task_id)
+
+            task_date = form.get('task_date', None)
+            if task_date is not None:
+                task.task_date = task_date
+
+            task_transition = form.get('task_transition', None)
+            if task_transition is not None:
+                task.task_transition = task_transition
+
+            ob_remove = form.get('ob_remove', None)
+            if ob_remove:
+                task.task_items = [
+                    it for it in task.task_items
+                    if it.to_id != int(ob_remove)
+                ]
+
+        return super(WFTasksOverviewView, self).__call__(*args, **kwargs)
