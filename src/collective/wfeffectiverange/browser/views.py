b7f9cc1b547088ffbda52ca5c0c5a2f8a12b98dd
Johannes Raggam <thetetet@gmail.com>
2017-08-28 11:40:06 +0200

    add wftaskrunner to immediately run a task

--- src/collective/wfeffectiverange/browser/views.py
+++ src/collective/wfeffectiverange/browser/views.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-from collective.wfeffectiverange.behaviors import IWFEffectiveRange
 from collective.wfeffectiverange.behaviors import IWFTask
+from collective.wfeffectiverange.browser.wftaskrunner import run_task
 from collective.wfeffectiverange.vocabulary import ExpiresTransitionSource
 from datetime import datetime
-from plone import api
-from plone.api.exc import InvalidParameterError
 from plone.protect.interfaces import IDisableCSRFProtection
 from Products.Five.browser import BrowserView
 from zope.interface import alsoProvides
@@ -53,13 +51,13 @@ class WFEffectiveRangeTicker(BrowserView):
             'has_effective_transition': True,
             # 'object_provides': IWFEffectiveRange.__identifier__
         }
-        for brain in api.content.find(**query):
+        for brain in plone.api.content.find(**query):
             obj = brain.getObject()
             if getattr(obj, 'effective_transition', None):
                 new_transition = obj.effective_transition
                 obj.effective_transition = None
                 obj._v_wfeffectiverange_ignore = True
-                api.content.transition(obj=obj, transition=new_transition)
+                plone.api.content.transition(obj=obj, transition=new_transition)
                 obj._v_wfeffectiverange_ignore = False
                 obj.reindexObject()
                 logger.info(
@@ -75,13 +73,13 @@ class WFEffectiveRangeTicker(BrowserView):
             'has_expires_transition': True,
             # 'object_provides': IWFEffectiveRange.__identifier__
         }
-        for brain in api.content.find(**query):
+        for brain in plone.api.content.find(**query):
             obj = brain.getObject()
             if hasattr(obj, 'expires_transition') and obj.expires_transition:
                 new_transition = obj.expires_transition
                 obj.expires_transition = None
                 obj._v_wfeffectiverange_ignore = True
-                api.content.transition(obj=obj, transition=new_transition)
+                plone.api.content.transition(obj=obj, transition=new_transition)
                 obj._v_wfeffectiverange_ignore = False
                 obj.reindexObject()
                 logger.info(
@@ -94,62 +92,17 @@ class WFEffectiveRangeTicker(BrowserView):
             logger.info('no autotransition done in this cycle')
         return 'triggered {0} autotransitions.'.format(triggered_something)
 
-
+        # Run Tasks
         infos = []
         warnings = []
-
         # Use timezone'd now.
         now = plone.app.event.base.localized_now(self.context)
-
-        # for WFTask objects
         query = {
             'start': {'query': now, 'range': 'max'},
             'object_provides': IWFTask.__identifier__
         }
-        for brain in api.content.find(**query):
+        for brain in plone.api.content.find(**query):
             task = brain.getObject()
-
-            for ref in task.task_items:
-                obj = ref.to_object
-
-                if not obj:
-                    # Invalid
-                    continue
-                if IWFEffectiveRange.providedBy(obj):
-                    # WFEffectiveRange objects referenced by a task will get
-                    # multi-edited by the task and run individually via the
-                    # code above.
-                    # Do not run them again.
-                    continue
-
-                transition = getattr(task, 'task_transition', None)
-                if not transition:
-                    # Incomplete
-                    continue
-
-                try:
-                    plone.api.content.transition(
-                        obj=obj,
-                        transition=transition
-                    )
-                    obj.reindexObject()
-                    infos.append(u'Task <a href="{0}">{1}</a> successfully run for object <a href="{2}">{3}</a>.'.format(  # noqa
-                        task.absolute_url(),
-                        task.title,
-                        obj.absolute_url(),
-                        obj.title
-                    ))
-                    logger.info(infos[-1])
-                except InvalidParameterError:
-                    warnings.append(u'Could not apply task <a href="{0}">{1}</a> with transform {2} for object <a href="{3}">{4}</a>.'.format(  # noqa
-                        task.absolute_url(),
-                        task.title,
-                        task.task_transition,
-                        obj.absolute_url(),
-                        obj.title
-                    ))
-                    logger.warn(warnings[-1])
-
-            IWFTask(task).task_transition = None
-            IWFTask(task).task_date = None
-            task.reindexObject()
+            infos_, warnings_ = run_task(task, include_wfer=False)
+            infos += infos_
+            warnings += warnings_
---
+++ src/collective/wfeffectiverange/browser/wftaskrunner.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from collective.wfeffectiverange.behaviors import IWFEffectiveRange
+from collective.wfeffectiverange.behaviors import IWFTask
+from plone.app.uuid.utils import uuidToObject
+from plone.protect.interfaces import IDisableCSRFProtection
+from Products.Five.browser import BrowserView
+from zope.interface import alsoProvides
+
+import json
+import logging
+import plone.api
+import plone.app.event
+
+
+logger = logging.getLogger('wfeffectiverange')
+
+
+def run_task(task, include_wfer=False):
+    """Run a task.
+    """
+    infos = []
+    warnings = []
+
+    for ref in task.task_items:
+        obj = ref.to_object
+
+        if not obj:
+            # Invalid
+            continue
+        if not include_wfer and IWFEffectiveRange.providedBy(obj):
+            # WFEffectiveRange objects referenced by a task will get
+            # multi-edited by the task and run individually via the
+            # code above.
+            # Do not run them again.
+            continue
+
+        transition = getattr(task, 'task_transition', None)
+        if not transition:
+            # Incomplete
+            continue
+
+        try:
+            plone.api.content.transition(
+                obj=obj,
+                transition=transition
+            )
+            if IWFEffectiveRange.providedBy(obj) and include_wfer:
+                if task.portal_type == 'WFTaskEffective':
+                    obj.effective_transition = None
+                else:
+                    obj.expires_transition = None
+            obj.reindexObject()
+            infos.append(u'Task <a href="{0}">{1}</a> successfully run for object <a href="{2}">{3}</a>.'.format(  # noqa
+                task.absolute_url(),
+                task.title,
+                obj.absolute_url(),
+                obj.title
+            ))
+            logger.info(infos[-1])
+
+        except plone.api.exc.InvalidParameterError:
+            warnings.append(u'Could not apply task <a href="{0}">{1}</a> with transform {2} for object <a href="{3}">{4}</a>.'.format(  # noqa
+                task.absolute_url(),
+                task.title,
+                task.task_transition,
+                obj.absolute_url(),
+                obj.title
+            ))
+            logger.warn(warnings[-1])
+
+    IWFTask(task).task_transition = None
+    IWFTask(task).task_date = None
+    task.reindexObject()
+
+    return infos, warnings
+
+
+class WFTaskRunnerView(BrowserView):
+
+    infos = []
+    warnings = []
+
+    def __call__(self, *args, **kwargs):
+        alsoProvides(self.request, IDisableCSRFProtection)
+        
+        task_uuid = self.request.form.get('task_uid', None)
+        if not task_uuid:
+            return
+
+        task = uuidToObject(task_uuid)
+        self.infos, self.warnings = run_task(task, include_wfer=True)
+
+        return super(WFTaskRunnerView, self).__call__(*args, **kwargs)
