34c21d37447aa56fa94b55c44b6a681905f20f99
Johannes Raggam <thetetet@gmail.com>
2017-08-29 10:19:54 +0200

    add special permissions

--- src/collective/wfeffectiverange/__init__.py
+++ src/collective/wfeffectiverange/__init__.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
+from AccessControl.SecurityInfo import ModuleSecurityInfo
+from Products.CMFCore.permissions import setDefaultRoles
 from zope.i18nmessageid import MessageFactory
 
 
 _ = MessageFactory('collective.wfeffectiverange')
+
+
+# Permissions
+security = ModuleSecurityInfo('collective.wfeffectiverange')
+
+security.declarePublic('collective.wfeffectiverange.addTaskFolder')
+setDefaultRoles('collective.wfeffectiverange: Add Task Folder', ('Manager', 'Site Administrator'))  # noqa
+
+security.declarePublic('collective.wfeffectiverange.addTask')
+setDefaultRoles('collective.wfeffectiverange: Add Task', ('Manager', 'Site Administrator', 'Reviewer'))  # noqa
