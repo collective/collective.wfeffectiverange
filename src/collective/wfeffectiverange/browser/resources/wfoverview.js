5b90b57d6fa643716c1bac0b5ad8b1f451713034
Johannes Raggam <thetetet@gmail.com>
2017-08-30 00:29:36 +0200

    clear the url after loading from url parameters

--- src/collective/wfeffectiverange/browser/resources/wfoverview.js
+++ src/collective/wfeffectiverange/browser/resources/wfoverview.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
 
     function basic_parameters (start_node) {
-        var wftype = $(start_node).closest('section').data('wftype');
-        var actionurl = $(start_node).closest('section').data('actionurl');
-        var uuid = $(start_node).closest('tr').data('uuid');
+        var $start_node = $(start_node);
+        var wftype = $start_node.closest('section').data('wftype');
+        var actionurl = $start_node.closest('section').data('actionurl');
+        var uuid = $start_node.closest('tr').data('uuid');
         return {
             wftype: wftype,
             actionurl: actionurl,
             uuid: uuid
         };
     }
+
+    // Remove the URL parameters
+    history.replaceState({} , '', window.location.href.split('?')[0]);
 
     $('select[name="transition"]').on('change', function(e) {
         var val = this.value || undefined;
