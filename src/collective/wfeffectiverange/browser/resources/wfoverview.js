51b8afe6c3850dcd79f9a1515526af202c733393
Johannes Raggam <thetetet@gmail.com>
2017-09-04 13:39:35 +0200

    do not wait for user interaction to initialize on change handlers - only wait for pickadate to be initialized

--- src/collective/wfeffectiverange/browser/resources/wfoverview.js
+++ src/collective/wfeffectiverange/browser/resources/wfoverview.js
@@ -14,22 +14,20 @@ $(document).ready(function() {
 
     // Remove the URL parameters
     history.replaceState({} , '', window.location.href.split('?')[0]);
-
-    $(document).one('click', function () {
-        // initialize inputs only when at least one click was made.
-        // pickadate initializes on load and thus would go into a reload loop
-        // if we do not wait for any user interaction first.
     
-        $('select[name="transition"]').on('change', function(e) {
-            var val = this.value || '';
-            var basic = basic_parameters(this);
+    $('select[name="transition"]').on('change', function(e) {
+        var val = this.value || '';
+        var basic = basic_parameters(this);
 
-            window.location.href = basic.actionurl
-                + '&wftype=' + basic.wftype
-                + '&transition=' + val
-                + '&uuid=' + basic.uuid;
-        });
+        window.location.href = basic.actionurl
+            + '&wftype=' + basic.wftype
+            + '&transition=' + val
+            + '&uuid=' + basic.uuid;
+    });
 
+    $('input[name="transition_date"]').one('init.pickadate.patterns', function(e) {
+        // initialize pickadate input listener only after pickadate was initialized.
+        // pickadate sets values on load and thus would go into a reload loop.
         $('input[name="transition_date"]').on('updated.pickadate.patterns', function(e) {
             var val = this.value || '';
             var basic = basic_parameters(this);
@@ -39,7 +37,6 @@ $(document).ready(function() {
                 + '&transition_date=' + val
                 + '&uuid=' + basic.uuid;
         });
-    
     });
 
 });
